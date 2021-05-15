372aa250b83a4ca38540e89ba47446631cfc202d
Steph Olmsted <olmsted.stephanie@gmail.com>
2021-05-14 22:11:49 -0700

    views 2nd commit

--- pythonclub2/club/urls.py
+++ pythonclub2/club/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, 
+from django.urls import path 
+from . import views
 
 urlpatterns = [
-
+   path('', views.index, name='index')
+   path('products/', views.products, name='products'),
 ]
--- pythonclub2/club/views.py
+++ pythonclub2/club/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product, TechType, Review
 # Create your views here.
+def index(request):
+    return render(request, 'club/index.html')
+
+def products(request):
+    product_list=Product.all()
+    return render(request), 'club/products.html', {'product_list': product_list}
